dd31459e0205c641e2594c9e2b74139a00699326
gauravsonii12 <gauravsonig63@gamil.com>
2026-09-10 10:48:04 +0530

    day6

---
+++ javascript/day6/index.js
@@ -0,0 +1,64 @@
+// // ES6 modern javascript
+
+// //Normal
+// // function greet(){
+// //   console.log("Don,t touch my laptop")
+// // }
+// // greet()
+
+// //Arow Function
+// // let warn=()=>{//this is arrow function you can use it with any variable it is you can say just another method of writting function.
+// //   console.log("Be careful monik")
+// // }
+// // warn()
+
+// //Template literals
+// // let name="Rohit";
+// // let std="12th";
+// // console.log(`My name is ${name} and I am studying in ${std}`)
+
+// function greet(){
+//    return "Don,t touch my laptop"
+//  }
+// greet()
+// let name="Rohit";
+// let std="12th";
+// console.log(`My name is ${greet()} and I am studying in ${std}` )
+
+
+// // let students=["Monik","mehul","prince","aaditya"];
+// // let [a,b]=students;
+// // console.log(a);
+// // console.log(b);
+
+
+// // Destructuring
+// // let students=["Monik","mehul","prince","aaditya"];
+// // let [a,,b]=students;
+// // console.log(b);
+
+// // let obj={
+// //   name:"Monki",
+// //   std:"nurdery",
+// // }
+// // let {name,std}=obj;
+// // console.log(name)
+
+// // spread vs rest
+// // console.log(Math.max(1,2,3,4,5,6))
+// // let arr=[1,2,3,4,5,6]
+// //  console.log(Math.max(...arr))
+// //  console.log(...arr)
+
+// // let obj={
+// //   name:"mahesh",
+// //   std:"nursery"
+// // }
+// // console.log({...obj,sub:"Hindi"})
+
+
+// //rest operators
+// // function sum(a,b,...rest){
+// //   console.log(rest)
+// // }
+// // sum(1,2,3,4,5,6,7,8)
